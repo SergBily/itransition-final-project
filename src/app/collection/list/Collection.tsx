2efa3bb7a96ff6404a143dc74e4c58d208a1b782
SergBily <89011743+SergBily@users.noreply.github.com>
2023-02-10 13:10:27 +0300

    feat: create collection dashboard

--- src/app/collection/list/Collection.tsx
+++ src/app/collection/list/Collection.tsx
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Card, CardActionArea, CardMedia, CardContent, Typography,
 } from '@mui/material';
 import CollectionStructure from '../../../shared/models/collectionStructure.model';
+import CollectionDashboard from './CollectionDashboard';
 
 interface CollectionProps {
-  payload: CollectionStructure
+  payload: CollectionStructure,
 }
 
 const Collection = ({ payload }: CollectionProps) => {
   const {
     img, topic, title, description,
   } = payload;
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovering(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovering(false);
+  };
 
   return (
-    <Card sx={{ maxWidth: 272, minHeight: 400, maxHeight: 400 }}>
+    <Card
+      sx={{
+        maxWidth: 272,
+        minHeight: 400,
+        maxHeight: 400,
+        position: 'relative',
+      }}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
       <CardActionArea>
         <CardMedia
           component="img"
@@ -38,6 +57,7 @@ const Collection = ({ payload }: CollectionProps) => {
           </Typography>
         </CardContent>
       </CardActionArea>
+      <CollectionDashboard onItem={isHovering} />
     </Card>
   );
 };
--- src/app/collection/list/CollectionDashboard.tsx
+++ src/app/collection/list/CollectionDashboard.tsx
@@ -1,9 +1,62 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Tooltip, IconButton, Box } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Paper from '@mui/material/Paper';
+import EditIcon from '@mui/icons-material/Edit';
+import gsap from 'gsap';
 
-const CollectionDashboard = () => {
-  console.log(1);
+interface CollectionDashboardProps {
+  onItem: boolean
+}
 
-  return (<div>collection Dashboard</div>);
+const CollectionDashboard = ({ onItem }: CollectionDashboardProps) => {
+  useEffect(() => {
+    if (onItem) {
+      gsap.fromTo(
+        '.dashboard',
+        { left: -105, duration: 0.5, ease: 'power1.inOut' },
+        { left: 15, duration: 0.5, ease: 'power1.inOut' },
+      );
+    } else {
+      gsap.fromTo(
+        '.dashboard',
+        { left: 15, duration: 0.5, ease: 'power1.inOut' },
+        { left: -105, duration: 0.5, ease: 'power1.inOut' },
+      );
+    }
+  }, [onItem]);
+
+  return (
+    <Paper
+      className="dashboard"
+      elevation={5}
+      sx={{
+        position: 'absolute',
+        top: 140,
+        left: -45,
+        backgroundColor: '#3ad2ff8d',
+
+      }}
+    >
+      <Box sx={{
+        width: 100,
+        display: 'flex',
+        justifyContent: 'space-between',
+      }}
+      >
+        <Tooltip title="Delete">
+          <IconButton color="error">
+            <DeleteIcon />
+          </IconButton>
+        </Tooltip>
+        <Tooltip title="Edit">
+          <IconButton color="warning">
+            <EditIcon />
+          </IconButton>
+        </Tooltip>
+      </Box>
+    </Paper>
+  );
 };
 
 export default CollectionDashboard;
