05d92127523ca265d1b2251324d66c1f0d208dfc
SergBily <89011743+SergBily@users.noreply.github.com>
2023-02-10 10:04:34 +0300

    feat: create collection item

--- src/app/collection/list/Collection.tsx
+++ src/app/collection/list/Collection.tsx
@@ -1,9 +1,45 @@
 import React from 'react';
+import {
+  Card, CardActionArea, CardMedia, CardContent, Typography,
+} from '@mui/material';
+import CollectionStructure from '../../../shared/models/collectionStructure.model';
 
-const Collection = () => {
-  console.log(1);
+interface CollectionProps {
+  payload: CollectionStructure
+}
 
-  return (<div>collection</div>);
+const Collection = ({ payload }: CollectionProps) => {
+  const {
+    img, topic, title, description,
+  } = payload;
+
+  return (
+    <Card sx={{ maxWidth: 272, minHeight: 400, maxHeight: 400 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="160"
+          image={img}
+          alt={title}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {topic}
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div">
+            {title}
+          </Typography>
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            sx={{ minHeight: 125, maxHeight: 125, overflow: 'scroll' }}
+          >
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
 };
 
 export default Collection;
---
+++ src/shared/models/collectionStructure.model.ts
@@ -0,0 +1,8 @@
+interface CollectionStructure {
+  img: string,
+  topic: string,
+  title: string,
+  description: string
+}
+
+export default CollectionStructure;
