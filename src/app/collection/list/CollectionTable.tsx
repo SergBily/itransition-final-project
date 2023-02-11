c88d53d58ae3bbf10cd97903caf3958245e33e57
SergBily <89011743+SergBily@users.noreply.github.com>
2023-02-11 22:17:39 +0300

    feat: implement user collection page

--- src/app/collection/list/CollectionTable.tsx
+++ src/app/collection/list/CollectionTable.tsx
@@ -1,9 +1,69 @@
+import Grid from '@mui/material/Grid';
 import React from 'react';
+import generateKey from '../../../shared/utils/UniqueKey';
+import Collection from './Collection';
+import img from '../../../assets/images/test.png';
+import CollectionCreator from './CollectionCreator';
+
+const testCollection = [
+  {
+    img,
+    topic: 'Books',
+    title: 'Lizard',
+    description: `Mongoose also supports setting timestamps on subdocuments. 
+    Keep in mind that createdAt and updatedAt for subdocuments represent`,
+  },
+  {
+    img,
+    topic: 'Alcohole',
+    title: 'Wiski',
+    description: `Mongoose also supports setting timestamps on subdocuments. 
+    Keep in mind that createdAt and updatedAt for subdocuments represent
+    Mongoose also supports setting timestamps on subdocuments. 
+    Keep in mind that createdAt and updatedAt for subdocuments represent`,
+  },
+  {
+    img,
+    topic: 'Mark',
+    title: 'PIlimmkk dkkdiifll',
+    description: `Mongoose also supports setting timestamps on subdocuments. 
+    Keep in mind that createdAt and updatedAt for subdocuments represent
+    Mongoose also supports setting timestamps on subdocuments. 
+    Keep in mind that createdAt and updatedAt for subdocuments represent
+    Mongoose also supports setting timestamps on subdocuments. 
+    Keep in mind that createdAt and updatedAt for subdocuments represent`,
+  },
+  {
+    img,
+    topic: 'Boots',
+    title: 'Lizard quntty',
+    description: `Mongoose also supports setting timestamps on subdocuments. 
+    Keep in mind that createdAt and updatedAt for subdocuments represent
+    Mongoose also supports setting timestamps on subdocuments. 
+    Keep in mind that createdAt and updatedAt for subdocuments represent
+    Mongoose also supports setting timestamps on subdocuments. 
+    Keep in mind that createdAt and updatedAt for subdocuments represent
+    Mongoose also supports setting timestamps on subdocuments. 
+    Keep in mind that createdAt and updatedAt for subdocuments represent
+    `,
+  },
+];
 
 const CollectionTable = () => {
   console.log(1);
 
-  return (<div>collection table</div>);
+  return (
+    <Grid container sx={{ gap: 2, justifyContent: 'center', mt: 4 }}>
+      <Grid item>
+        <CollectionCreator />
+      </Grid>
+      {testCollection.map((colection) => (
+        <Grid item key={generateKey()}>
+          <Collection payload={colection} />
+        </Grid>
+      ))}
+    </Grid>
+  );
 };
 
 export default CollectionTable;
