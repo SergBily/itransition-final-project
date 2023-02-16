8319e2d898b1439f789c7f1046046966d32e4436
SergBily <89011743+SergBily@users.noreply.github.com>
2023-02-16 21:45:41 +0300

    refactor: collection form field

---
+++ src/app/collection/creation/collectionFormField/CollectionFormField .tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import {
+  Grid, Typography, Box,
+} from '@mui/material';
+import { OverridableComponent } from '@mui/material/OverridableComponent';
+import { SvgIconTypeMap } from '@mui/material/SvgIcon';
+import { FormattedMessage } from 'react-intl';
+import classNames from 'classnames';
+import styles from './styles.module.scss';
+
+interface CollectionFormFieldProps {
+  label: string,
+  Icon: OverridableComponent<SvgIconTypeMap<{}, 'svg'>>
+  children: React.ReactNode
+}
+
+const CollectionFormField = ({ label, Icon, children }: CollectionFormFieldProps) => {
+  const rootClass = classNames(styles.root, {
+    [styles.positionStart]: label === 'description' || label === 'image',
+    [styles.positionCenter]: label !== 'description',
+  });
+
+  return (
+    <Grid
+      container
+      className={rootClass}
+    >
+      <Grid item xs={12} sm={2}>
+        <Box
+          component="div"
+          className={styles.title}
+        >
+          <Icon />
+          <Typography
+            variant="h6"
+            color="initial"
+            className={styles.text}
+          >
+            <FormattedMessage id={`app.collection.new.field.${label}`} />
+          </Typography>
+        </Box>
+      </Grid>
+      <Grid
+        item
+        sm={label === 'description' ? 9 : 4}
+        xs={11}
+      >
+        {children}
+      </Grid>
+    </Grid>
+  );
+};
+
+export default CollectionFormField;
